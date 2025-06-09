ddf20b98cd0b8cf9ae04ab1c568a081f411a40eb
Thu Than <than@uni.minerva.edu>
2025-06-09 13:07:35 +0700

    add insertion sort in C

---
+++ c/insertion_sort.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void insertion_sort(int s[], int n)
+{
+    int i, j;
+
+    for (i = 1; i < n; i++)
+    {
+        j = i;
+        while ((j > 0) && (s[j] < s[j - 1]))
+        {
+            swap(&s[j], &s[j - 1]);
+            j = j - 1;
+        }
+    }
+}
